ff890cbd904fa7d71cbaf00db79ce5b3e14ded0e
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2022-03-16 22:44:20 +0300

    Refactor entities

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -1,13 +1,19 @@
-﻿namespace Core.Entities.Concrete
+﻿using System;
+
+namespace Core.Entities.Concrete
 {
-    public class User : IEntity
+    public class User : IAuditedEntity, ISoftDeleteEntity
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string GsmNumber { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public bool Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
     }
 }
--- Core/Entities/Concrete/UserOperationClaim.cs
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -1,9 +1,13 @@
-﻿namespace Core.Entities.Concrete
+﻿using System;
+
+namespace Core.Entities.Concrete
 {
-    public class UserOperationClaim : IEntity
+    public class UserOperationClaim : IAuditedEntity
     {
         public int Id { get; set; }
         public int UserId { get; set; }
         public int OperationClaimId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
     }
 }
